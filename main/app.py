1ce7788a1e770633ff7d46151ef3170fc3b5a4c8
ocelot <probablyanocelot@gmail.com>
2023-04-03 17:12:36 -0500

    cleaning

--- main/app.py
+++ main/app.py
@@ -9,8 +9,6 @@ import requests
 # from producer import publish
 
 app = Flask(__name__)
-# app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:root@db/main'  # :root
-# :5432 , :33068
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://test:test@db:5432/adv_lnd'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
@@ -20,24 +18,13 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
-@dataclass
-class Product(db.Model):
-    id: int
-    title: str
-    image: str
+# @dataclass
+# class ProductUser(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     user_id = db.Column(db.Integer)
+#     product_id = db.Column(db.Integer)
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
-    title = db.Column(db.String(200))
-    image = db.Column(db.String(200))
-
-
-@dataclass
-class ProductUser(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer)
-    product_id = db.Column(db.Integer)
-
-    UniqueConstraint('user_id', 'product_id', name='user_product_unique')
+#     UniqueConstraint('user_id', 'product_id', name='user_product_unique')
 
 
 @dataclass
@@ -48,11 +35,6 @@ class JsonObject(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.Text())
-
-
-@app.route('/api/products')
-def index():
-    return jsonify(Product.query.all())
 
 
 @app.route('/api/bank/wipe')
@@ -67,9 +49,7 @@ def wipe():
 
 @app.route('/api/bank', methods=['POST', 'GET'])
 def bank():
-    # content = request.json
-    # content = {'me': 'irritable'}
-    # return jsonify(JsonObject.query.all())
+
     if request.method == 'GET':
         try:
             return jsonify(JsonObject.query.all())
@@ -90,37 +70,30 @@ def bank():
         return jsonify({
             'message': 'success'
         })
-    # return jsonify(JsonObject.query.all())
 
 
 @app.route('/api/bank/<int:id>', methods=['GET'])
 def bank_by_id(id):
     return jsonify(JsonObject.query.get(id))
 
-# @app.route('/api/add_message/<uuid>', methods=['GET', 'POST'])
-# def add_message(uuid):
-#     content = request.json
-#     print(content['mytext'])
-#     return jsonify({"uuid":uuid})
 
+# @app.route('/api/products/<int:id>/like', methods=['POST'])
+# def like(id):
+#     req = requests.get('http://docker.for.mac.localhost:8000/api/user')
+#     json = req.json()
 
-@ app.route('/api/products/<int:id>/like', methods=['POST'])
-def like(id):
-    req = requests.get('http://docker.for.mac.localhost:8000/api/user')
-    json = req.json()
+#     try:
+#         productUser = ProductUser(user_id=json['id'], product_id=id)
+#         db.session.add(productUser)
+#         db.session.commit()
 
-    try:
-        productUser = ProductUser(user_id=json['id'], product_id=id)
-        db.session.add(productUser)
-        db.session.commit()
+#         publish('product_liked', id)
+#     except:
+#         abort(400, 'You already liked this product')
 
-        publish('product_liked', id)
-    except:
-        abort(400, 'You already liked this product')
-
-    return jsonify({
-        'message': 'success'
-    })
+#     return jsonify({
+#         'message': 'success'
+#     })
 
 
 if __name__ == '__main__':
